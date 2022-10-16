2210b517dbbc69c38fec763843291e568caa9f8f
arturmingaleev <art.mingaleev@yandex.ru>
2022-10-16 20:10:41 +0300

    Lesson 3 - Мигрировал на ViewPager2

--- app/src/main/java/ru/mingaleev/materialyou/view/navigation/ViewPagerFragment.kt
+++ app/src/main/java/ru/mingaleev/materialyou/view/navigation/ViewPagerFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import ru.mingaleev.materialyou.databinding.FragmentViewPagerBinding
 
 
@@ -22,8 +24,23 @@ class ViewPagerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.viewPager.adapter = activity?.let { ViewPagerAdapter(it.supportFragmentManager) }
-        binding.tabLayout.setupWithViewPager(binding.viewPager)
+        binding.viewPager.adapter = activity?.let { ViewPager2Adapter(it) }
+
+        bindTabLayout()
+    }
+
+    private fun bindTabLayout() {
+        TabLayoutMediator(binding.tabLayout, binding.viewPager, object : TabLayoutMediator.TabConfigurationStrategy {
+            override fun onConfigureTab(tab: TabLayout.Tab, position: Int) {
+                tab.text = position.toString()
+                return when (position) {
+                    0 -> tab.text = "Земля"
+                    1 -> tab.text = "Марс"
+                    2 -> tab.text = "Система"
+                    else -> tab.text = "Земля"
+                }
+            }
+        }).attach()
     }
 
     override fun onDestroyView() {
