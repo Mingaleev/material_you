f1a905c71daf77b465a2951d8b35fd4b7096edaf
arturmingaleev <art.mingaleev@yandex.ru>
2022-11-04 13:08:14 +0300

    Lesson 5 - Реализовал RecyclerView и использовал анимацию Explode при нажатии на item

--- app/src/main/java/ru/mingaleev/materialyou/view/AnimationFragment.kt
+++ app/src/main/java/ru/mingaleev/materialyou/view/AnimationFragment.kt
@@ -1,12 +1,16 @@
 package ru.mingaleev.materialyou.view
 
 import android.os.Bundle
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.transition.*
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.transition.Explode
+import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
+import ru.mingaleev.materialyou.R
 import ru.mingaleev.materialyou.databinding.FragmentAnimationBinding
 
 class AnimationFragment : Fragment() {
@@ -27,19 +31,37 @@ class AnimationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonAnimation.setOnClickListener() {
-            flagVisibilityTextView = !flagVisibilityTextView
-            val myAutoTransition = TransitionSet()
-            myAutoTransition.ordering = TransitionSet.ORDERING_TOGETHER
-            val slide = Slide(Gravity.START)
-            val changeBounds = ChangeBounds()
+        flagVisibilityTextView = !flagVisibilityTextView
 
-            myAutoTransition.duration = 800L
-            myAutoTransition.addTransition(changeBounds)
-            myAutoTransition.addTransition(slide)
+        binding.recyclerView.adapter = Adapter()
+    }
 
-            TransitionManager.beginDelayedTransition(binding.transitionContainer, myAutoTransition)
-            binding.textViewAnimation.visibility = if (flagVisibilityTextView) View.VISIBLE else View.GONE
+    inner class Adapter : RecyclerView.Adapter<ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return MyViewHolder(
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.fragment_animation_explode_list_item, parent, false) as View
+            )
         }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.itemView.setOnClickListener {
+                val myAutoTransition = TransitionSet()
+                val explode = Explode()
+
+                myAutoTransition.addTransition(explode)
+                myAutoTransition.duration = 1000L
+
+                TransitionManager.beginDelayedTransition(binding.transitionContainer, myAutoTransition)
+                binding.recyclerView.adapter = null
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return 32
+        }
+
+        inner class MyViewHolder(view: View) : ViewHolder(view)
+
     }
 }
